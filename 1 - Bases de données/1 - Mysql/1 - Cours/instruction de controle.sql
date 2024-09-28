57a9a0a7b7844f2ad52f7a80689edee7e1ebe599
rahmouni oussama <ousrah@hotmail.com>
2024-09-28 11:40:30 +0100

    les instructions de controle

---
+++ 1 - Bases de données/1 - Mysql/1 - Cours/instruction de controle.sql
@@ -0,0 +1,297 @@
+#Les instructions de controle sous mysql
+
+# block d'instruction mysql
+
+drop function if exists hello;
+delimiter $$
+create function hello()
+	returns varchar(50)
+    deterministic
+begin
+	return 'hi';
+end $$
+delimiter ;
+
+select hello();
+
+# la declaration
+
+
+
+drop function if exists la_declaration;
+delimiter $$
+create function la_declaration()
+	returns int
+    deterministic
+begin
+	declare a int default 3;
+    return a;
+end $$
+delimiter ;
+
+select la_declaration();
+
+drop function if exists addition;
+delimiter $$
+create function addition()
+	returns int
+    deterministic
+begin
+	declare a int default 3;
+    declare b int default 5;
+    return a + b;
+end $$
+delimiter ;
+
+select addition();
+
+# l'affectation
+
+drop function if exists addition;
+delimiter $$
+create function addition()
+	returns int
+    deterministic
+begin
+	declare a,b,c int;
+    set a = 3;
+    set b = 5;
+    set c = 8;
+    return c;
+end $$
+delimiter ;
+
+
+select addition();
+
+
+
+drop function if exists addition;
+delimiter $$
+create function addition()
+	returns int
+    deterministic
+begin
+	declare a,b,c int;
+    set a = 3;
+    select 5 into b;
+    select a+b into c;
+    return c;
+end $$
+delimiter ;
+select addition();
+
+
+
+
+drop function if exists addition;
+delimiter $$
+create function addition()
+	returns int
+    deterministic
+begin
+	declare a,b,c int;
+    set a = 3, b=5;
+    select a+b into c;
+    return c;
+end $$
+delimiter ;
+select addition();
+
+
+
+
+
+#passage de paramètres à la fonction
+drop function if exists addition;
+delimiter $$
+create function addition(a int, b int)
+	returns int
+    deterministic
+begin
+	declare c int;
+    set c = a+b;
+    return c;
+end $$
+delimiter ;
+select addition(3,5);
+
+
+
+# les couditions
+
+
+drop function if exists compare;
+delimiter $$
+create function compare(a int, b int)
+	returns int
+    deterministic
+begin
+	declare c int;
+    if a > b then
+		set c = a;
+	else                       -- elseif est disponible aussi
+		set c = b;
+	end if;
+    return c;
+end $$
+delimiter ;
+select compare(13,5);
+
+
+# exercice 1 
+# ecrire une fonction qui compare troi entiers et qui affiche le plus grand
+drop function if exists compare;
+delimiter $$
+create function compare(a int, b int,c int)
+ returns int 
+ deterministic
+begin
+	declare d int;
+    if a>=b and a>=c then
+		return a;
+	elseif b>=a and b>=c then
+		return b;
+	else 
+		return c;
+    end if;
+    
+end $$
+delimiter ;
+
+select compare(1,2,3); #7 operations
+select compare(1,3,2); #7 operations
+select compare(3,1,2); #4 operations
+
+
+drop function if exists compare;
+delimiter $$
+create function compare(a int, b int,c int)
+ returns int 
+ deterministic
+begin
+	declare max int;
+    set max = a;
+    if b>max then
+		set max = b;
+	end if;
+	if c>max then
+		return c;
+	end if;
+    return max;
+    
+end $$
+delimiter ;
+
+select compare(1,2,3); #5 operations
+select compare(1,3,2); #5 operations
+select compare(3,1,2); #4 operations
+
+
+
+drop function if exists compare;
+delimiter $$
+create function compare(a int, b int,c int)
+ returns int 
+ deterministic
+begin
+	
+    if a>b then
+		if a>c then
+			return a;
+		else
+			return c;
+		end if;
+	else
+		if b>c then
+			return b;
+		else
+			return c;
+		end if;
+	end if;
+end $$
+delimiter ;
+
+select compare(1,2,3); #3 operations
+select compare(1,3,2); #3 operations
+select compare(3,1,2); #3 operations
+
+
+
+
+
+
+#exercice 2 :
+/*
+un patron décide de participer aux prix de repas de ces employer 
+il instaure les règles suivantes :
+le pourcentage de la participation par defaut est 20% du prix de repas
+si le salaire est inférieur à 2500 dh le taux est augmenté de 15%
+si l'employé est marié le taux est augmenté de 5%
+pour chaque enfant a charge le taux est augmenté de 10%
+le plafond maximal est 60%
+on souhaite ecrire une fonction qui reçoit tous les paramètres 
+et qui affiche le montant de la participation selon 
+le prix de repas acheté par l'employé
+*/
+
+
+
+
+
+
+#exercice 3
+/*
+ecrire une fonction qui permet de resoudre une equation premier degrès
+Ax+B=0
+
+3x+2=0
+
+Rappels mathématique
+si A = 0 et B = 0  x = R
+si A = 0 et B <> 0 x = impossible
+si A <>  0 x = -b/a
+*/
+
+
+#exercice 4
+/*
+ecrire une fonction qui permet de resoudre une equation deuxième degrès
+Ax²+Bx+C=0
+2x²+3x+0=0
+
+Rappels mathématique
+si A = 0 et B = 0 et C = 0  x1 = x2 = R
+si A = 0 et B = 0  et C <> 0 x1 = x2 = impossible
+si A = 0 et B <> 0  x= -c/b
+si A <> 0 
+	 delta  = B²-4AC
+     si delta < 0 x1 = impossible
+     si delta = 0 x1 = x2 = -b/2a
+      si delta > 0 x1 = (-b-racine(delta))/2a  et x2 =  (-b+racine(delta))/2a   
+    */
+
+
+# les boucles
+
+
+
+
+#les fonctions
+
+
+#les procedures stockées
+
+# les tables temporaires
+
+# les triggers (les declencheurs)
+
+# la gestion des erreurs
+
+# les transactions
+
+# les curseurs
+
+# la gestion des utilisateurs
+
+# la sauvegarde et la restauration
--- 1 - Bases de données/1 - Mysql/2 - Exercices/courses hyppiques/select.sql
+++ 1 - Bases de données/1 - Mysql/2 - Exercices/courses hyppiques/select.sql
@@ -1,5 +1,5 @@
 select ceiling(rand()*18);
-
+use courses201;
 -- A.	la liste de tous les chevaux.
 
 select * from cheval;
@@ -84,14 +84,91 @@ order by classement asc;
 
 -- D.	la liste des jockeys qui ont monté le cheval "black" durant tout son historique
 
+select  distinct jockey.id_jockey, nom_jockey,prenom_jockey
+from jockey
+			join participe using (id_jockey)
+			join cheval using (id_cheval)
+where nom_cheval ='black';
 
 
+select jockey.id_jockey, nom_jockey,prenom_jockey
+from jockey 
+where id_jockey in ( select distinct id_jockey 
+					from participe 
+                    where id_cheval in (select id_cheval 
+										from cheval 
+                                        where nom_cheval = "black ")
+					);
 
+with j as (select distinct id_jockey 
+					from participe 
+                    where id_cheval in (select id_cheval 
+										from cheval 
+                                        where nom_cheval = "black "))
+select jockey.* from jockey join j using (id_jockey);
 
 -- E.	Le cheval qui a remporté le plus grand nombre de compétitions
 
+with t1 as (select id_cheval, count(classement) as nb from participe
+where classement = 1
+group by id_cheval),
+t2 as (select max(nb) as nb from t1)
+select cheval.* 
+	from cheval  join t1 using(id_cheval) 
+    join t2 using(nb) ;
+ 
+
+
 -- F.	Les parents du cheval qui a remporté le plus grand nombre de compétitions
+
+-- tout d'abord je vais faire une requtte qui permet de recupérer les parents de n'importe quel cheval
+
+select fils.id_cheval as id_cheval, fils.nom_cheval as nom_fils, pere.id_cheval as id_parent, pere.nom_cheval as nom_parent
+from cheval fils join parent using (id_cheval)
+join cheval pere on parent.id_parent = pere.id_cheval
+where fils.id_cheval = 1;
+
+-- je vais chercher a faire un lien entre la dernière quette et celle qui permet de récuprer le cheval qui a remporté le plus grand nombre de competitions.
+
+with t1 as (select id_cheval, count(classement) as nb from participe
+where classement = 1
+group by id_cheval),
+t2 as (select max(nb) as nb from t1)
+select fils.id_cheval as id_cheval, fils.nom_cheval as nom_fils, pere.id_cheval as id_parent, pere.nom_cheval as nom_parent
+from cheval fils join parent using (id_cheval)
+join cheval pere on parent.id_parent = pere.id_cheval
+join t1  on fils.id_cheval = t1.id_cheval
+join t2 using (nb)
+;
+
+
 
 -- G.	Le montant total remporté par 'black' dans toutes les compétitions qu'il a remporté
 
+
+select sum(dotation) from participe 
+join cheval using (id_cheval) 
+join session using(id_session)
+where nom_cheval = 'black' and classement = 1
+;
+
+
 -- H.	La catégorie que le cheval 'black' remporte le plus
+with t1 as (
+			select nom_categorie, count(id_categorie) nb from
+			cheval join participe using (id_cheval)
+			join session using(id_session)
+			join course using(id_course)
+			join categorie using (id_categorie)
+			where nom_cheval = 'black'
+			and classement = 1
+			group by nom_categorie
+            ),
+t2 as		(
+				select max(nb) nb from t1
+			) 
+select * from t1 join t2 using (nb);
+
+
+
+
